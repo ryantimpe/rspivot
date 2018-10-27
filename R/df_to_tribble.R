0414395d4f71b5d317c5a8f0ffc9392ee23c6bb9
ryantimpe <ryan.timpe@gmail.com>
2018-10-27 17:48:42 -0400

    Moving quicktibble package here. Some functions can be used in both addins

---
+++ R/df_to_tribble.R
@@ -0,0 +1,65 @@
+#' Convert a reactive data frame in Shiny to a tribble for editor
+#'
+#' Convert a reactive data frame in Shiny to a tribble for editor
+#' Mostly for use inside Shiny RStudio Addins
+#'
+#' @param .data A data frame to convert
+#' @param .name Optional name for tribble output
+#' @return Text sting that can be read as a tribble in R
+#' @export
+
+df_to_tribble <- function(.data, .name = NULL){
+  dat <- .data %>%
+    dplyr::mutate_if(is.factor, as.character) %>%
+    #Add single quotes to each character...
+    dplyr::mutate_if(is.character, dplyr::funs(ifelse(is.na(.), NA, paste0("'", ., "'")))) %>%
+    dplyr::mutate_all(dplyr::funs(ifelse(is.na(.), "", .)))
+
+  #Width of each column
+  op_widths <- 1:ncol(dat) %>%
+    purrr::map_int(function(i){
+      max(max(nchar(dat[, i])), nchar(names(dat)[i]))
+    })
+
+  #Header
+  op_names <- 1:ncol(dat) %>%
+    purrr::map_chr(function(i){
+      leading_spaces <- strrep(" ", op_widths[i] - nchar(names(dat)[i]) + 1)
+      paste0(leading_spaces, "~", names(dat)[i])
+    }) %>%
+    paste(collapse = ",")
+
+  #Rows
+  op_rows <- 1:nrow(dat) %>%
+    purrr::map_chr(function(rw){
+
+      dat.row <- dat[rw, ]
+
+      1:ncol(dat.row) %>%
+        purrr::map_chr(function(i){
+          leading_spaces <- strrep(" ", op_widths[i] - nchar(dat.row[i]) + 2)
+          paste0(leading_spaces, dat.row[i])
+        }) %>%
+        paste(collapse = ",") %>%
+        stringr::str_replace_all("  ,", "NA,")
+    }) %>%
+    paste(collapse = ", \n")
+
+  #Tribble
+  op_trib <- paste("tibble::tribble(",
+                   paste0(op_names, ","),
+                   op_rows,
+                   ")",
+                   sep = "\n"
+                   )
+
+  if(!is.null(.name)){
+    op_trib <- paste(
+      make.names(.name), "<-", op_trib
+    )
+  }
+
+  return(op_trib)
+}
+
+
---
+++ R/quicktibble.R
@@ -0,0 +1,311 @@
+#' RStudio addin to quickly build small tibbles from an existing data frame or array.
+#'
+#' RStudio addin to quickly build small tibbles from an existing data frame or array.
+#' Useful with dplyr::left_join().
+#'
+#' @param .data Optional data to initialize the new table. If a data frame, then the selection will default to this data frame. If an array, then the input will be the array.
+#' @param .name Optional name for tribble output
+#' @return Text sting that can be read as a tribble in R
+#' @export
+
+quicktibble <- function(.data = NULL){
+  ################
+  # Non-Reactive functions ----
+  ################
+  if(is.null(.data)){
+    input.data <- NULL
+    data.check <- TRUE
+  } else if(is.data.frame(.data)){
+    input.data <- deparse(substitute(.data))
+    data.check <- TRUE
+  } else if(is.vector(.data) || is.factor(.data)){
+    input.data <- deparse(substitute(.data))
+    data.check <- FALSE
+  } else {
+    stop("Input data should be a data frame or a vector")
+  }
+
+  appCSS <-
+    "#selInputData   ~ .selectize-control.single .selectize-dropdown [data-value='Provided Array'] { color: blue }
+     #selInputColumn ~ .selectize-control.single .selectize-dropdown [data-value=`Provided Array'] { color: blue }"
+
+  collapse_col2 <- function(index, data){
+    #Check to see if its a numeric column...
+    check.num <- suppressWarnings(sum(is.na(as.numeric(as.character(data[, index][!is.na(data[, index])])))) > 0)
+
+    if(!check.num){
+      op <- paste0(names(data)[index], " = c( ", paste0(data[, index], collapse = ", "), ")")
+    } else {
+      op <- paste0(names(data)[index], " = c( '", paste0(data[, index], collapse = "', '"), "')")
+    }
+
+    #Replace 'NA'
+    op <- gsub("'NA'", "NA", op)
+
+    return(op)
+  }
+
+  ################
+  # UI ----
+  ################
+  ui <- miniUI::miniPage(
+    tags$head(tags$style(HTML(appCSS))),
+    miniUI::gadgetTitleBar("quicktibble"),
+    conditionalPanel(
+      condition = "input.selInputBuild == 0",
+      miniUI::miniButtonBlock(
+        fluidRow(
+          column(width = 3,
+                 selectInput("selInputData", label = "From data frame",
+                             choices = c("None",
+                                         names(eapply(.GlobalEnv,is.data.frame))[unlist(eapply(.GlobalEnv,is.data.frame))]),
+                             selected = input.data),
+                 conditionalPanel(
+                   condition = "input.selInputData != 'Provided Array'",
+                   selectInput("selInputColumn", label = "From column",
+                               choices = NULL)
+                 ),
+                 conditionalPanel(
+                   condition = "input.selInputData == 'Provided Array'",
+                   textInput("selInputArray", label = "Array name",
+                              value = gsub("$", "_", deparse(substitute(.data)), fixed=TRUE))
+                 )
+          ),
+          column(width = 3,
+                 strong("Column names"),
+                 helpText("Leave blank to not include"),
+                 textInput("setCol2", label = NULL, value ="Col2", placeholder = "Column 2 name"),
+                 textInput("setCol3", label = NULL, value ="",     placeholder = "Column 3 name")
+
+          ),
+          column(width = 3,
+                 strong("Column names"),
+                 helpText("---"),
+                 textInput("setCol4", label = NULL, value ="",     placeholder = "Column 4 name"),
+                 textInput("setCol5", label = NULL, value ="",     placeholder = "Column 5 name")
+          ),
+          column(width = 3,
+                 checkboxInput("setWeight", "Weight column?", value = FALSE),
+                 checkboxInput("setUnique", "Unique values", value = TRUE),
+                 hr(),
+                 actionButton("selInputBuild", label = "Create")
+          )
+        )
+      )
+    ),
+    conditionalPanel(
+      condition = "input.selInputBuild != 0",
+      miniUI::miniButtonBlock(
+        textInput("setOPname", label = "Output table name", value = "QuickTibble", placeholder = "Output Name"),
+        conditionalPanel(
+          condition = "input.setWeight == true",
+          p("When complete, scale weights?"),
+          radioButtons("setWeightScale", label = NULL,
+                       choices = "none",
+                       selected = "none",
+                       inline = TRUE),
+          helpText("Choose a column name to scale each group to 100%.")
+        )
+      )
+    ),
+    miniUI::miniContentPanel(
+      rhandsontable::rHandsontableOutput("hot"),
+      verbatimTextOutput("opPreview")
+    )
+
+  ) #End UI
+
+  ################
+  # Server ----
+  ################
+
+  server <- function(input, output, session) {
+
+    ###
+    # Update selects
+    ###
+    # DF selection
+    observe({
+      if(!data.check){
+        updateSelectInput(session, "selInputData",
+                          choices = c("Provided Array", "None",
+                                      names(eapply(.GlobalEnv,is.data.frame))[unlist(eapply(.GlobalEnv,is.data.frame))]),
+                          selected = "Provided Array")
+      }
+    })
+    # Column selection
+    observe({
+      if(input$selInputData == "None"){
+        updateSelectInput(session, "selInputColumn",
+                          choices = "Select a data frame")
+      } else if(input$selInputData == "Provided Array"){
+        updateSelectInput(session, "selInputColumn",
+                          choices = "Provided Array")
+      } else{
+        updateSelectInput(session, "selInputColumn",
+                          choices = names(get(input$selInputData)))
+      }
+    })
+
+    # Weight scaling
+    observe({
+      wghtChoices <- c(input$setCol2, input$setCol3, input$setCol4, input$setCol5)
+      wghtChoices <- wghtChoices[wghtChoices != ""]
+
+      updateRadioButtons(session, "setWeightScale",
+                         choices = c("No"="no", "All to 100%"="total", input$selInputColumn, wghtChoices),
+                         selected = "no",
+                         inline = TRUE)
+    })
+
+    ###
+    # Build starting data frame
+    ###
+    df.start <- eventReactive(input$selInputBuild, {
+
+      #If starting is the provided array...
+      if(!data.check && input$selInputColumn == "Provided Array"){
+        starting.column <- as.character(.data)
+      } else {
+        starting.column <- tibble::as.tibble(get(input$selInputData)[, input$selInputColumn]) %>% dplyr::pull()
+      }
+
+      if(input$setUnique){
+        starting.column <- unique(starting.column)
+      }
+
+      dat <- tibble::tibble(starting.column, V2 = "")
+
+      # #If starting is the provided array...
+      # if(!data.check && input$selInputColumn == "Provided Array"){
+      #   # names(dat)[1] <- gsub("[^[:alnum:] ]", "", input$selInputArray)
+      #   names(dat)[1] <- "Colllll"
+      # } else {
+        names(dat)[1] <- input$selInputColumn
+      # }
+
+      names(dat)[2] <- input$setCol2
+
+      #Additional columns
+      if(input$setCol3 != ""){
+        dat <- dat %>%
+          dplyr::mutate(.col = "")
+        names(dat)[names(dat) == ".col"] <- input$setCol3
+      }
+      if(input$setCol4 != ""){
+        dat <- dat %>%
+          dplyr::mutate(.col = "")
+        names(dat)[names(dat) == ".col"] <- input$setCol4
+      }
+      if(input$setCol5 != ""){
+        dat <- dat %>%
+          dplyr::mutate(.col = "")
+        names(dat)[names(dat) == ".col"] <- input$setCol5
+      }
+
+      #Weight Column
+      if(input$setWeight){
+        dat <- dat %>%
+          dplyr::mutate(.Weight = 1)
+      }
+
+      return(dat)
+    }, ignoreInit = TRUE, ignoreNULL = TRUE)
+
+    output$hot = rhandsontable::renderRHandsontable({
+      if (!is.null(input$hot)) {
+        DF = rhandsontable::hot_to_r(input$hot)
+      } else {
+        DF = df.start()
+      }
+
+      rhandsontable::rhandsontable(DF, height = (nrow(DF)+2)*25) %>%
+        rhandsontable::hot_table(highlightCol = TRUE, highlightRow = TRUE) %>%
+        rhandsontable::hot_context_menu(allowRowEdit = TRUE, allowColEdit = TRUE) %>%
+        rhandsontable::hot_col(col = input$selInputColumn,  strict = FALSE, allowInvalid = TRUE)
+    })
+
+    # Finalize the output
+
+    #Scale Weights
+    tbScaled <- reactive({
+      hot = input$hot
+      if (!is.null(hot)) {
+        dat <- rhandsontable::hot_to_r(hot) %>%
+          dplyr::mutate_if(is.factor, as.character) %>%
+          #Replace blanks each NA
+          dplyr::mutate_all(dplyr::funs(ifelse(. == "", NA, .)))
+
+        #Check to see if weight column
+        if(input$setWeight){
+          #Scale to total...
+          if(input$setWeightScale == "total"){
+            dat <- dat %>%
+              dplyr::mutate(.Weight = round(.Weight / sum(.Weight, na.rm = TRUE), 5))
+            #Scale to a column....
+          } else if(input$setWeightScale != "no"){
+            dat <- dat %>%
+              dplyr::group_by(!!!rlang::syms(c(input$setWeightScale))) %>%
+              dplyr::mutate(.Weight = round(.Weight / sum(.Weight, na.rm = TRUE), 5)) %>%
+              dplyr::ungroup() %>%
+              as.data.frame()
+          } else {dat <- dat} #Other don't scale
+        }
+
+      } else {dat <- df.start()}
+
+      return(dat)
+    })
+
+    # Preview the output
+    codePreview <- reactive({
+      hot = input$hot
+
+      if (!is.null(hot)) {
+
+        opName <- if(input$setOPname == ""){"QuickTibble"}else{input$setOPname}
+        dat <- tbScaled()
+
+        if(names(dat)[1] == "Provided Array"){
+          names(dat)[1] <- gsub("[^[:alnum:] ]", "", input$selInputArray)
+        }
+
+        op <- df_to_tribble(dat, opName)
+
+      } else {op <- "Print Preview"}
+
+      return(op)
+
+    })
+    output$opPreview <- renderText({
+      return(codePreview())
+    })
+    # output$opPreview <- renderTable({
+    #   return(tbScaled())
+    # })
+
+    # Listen for 'done' events.
+    observeEvent(input$done, {
+      hot = isolate(input$hot)
+      if (!is.null(hot)) {
+        rstudioapi::insertText(Inf, codePreview())
+      }
+      stopApp()
+    })
+
+    session$onSessionEnded(function() {
+      stopApp()
+    })
+
+  } #End Server
+
+  ####
+  # Addin settings ----
+  ####
+
+  viewer <- dialogViewer(paste("quicktibble"), width = 1200, height= 1200)
+
+
+  runGadget(ui, server, viewer = viewer)
+}
+
